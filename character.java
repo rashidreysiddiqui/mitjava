066b7712dacf8d0266a8628675b943693392ee96
rashid <rashidsiddqui07@gmail.com>
2020-05-07 15:39:11 +0530

    characters

---
+++ character.java
@@ -0,0 +1,36 @@
+// hw
+// unicode ascii
+// 256
+// ascii -> 
+// utf-8-> utf-16 utf-32
+// character ->2 bytes->16 bit-> 0 to 65535
+// 64206
+// 61->97->b
+// escape chracters
+// \n-> newline I
+//  \t-> htab
+//  \r-> carriage return
+//  \f-> formfeed
+// \b-> backspace
+//  \ '-> single qoutes II
+//  \" -> double qoutes II
+//  0-256-> ascii -> u
+// a->97
+// a->97
+// 1_000_000_000
+// 10_00_00_000
+
+class character{
+
+
+
+public static void main(String args[]){
+
+// char ch='\n';
+// double d=1_2__3.05__6;;
+System.out.println(b);
+
+}
+
+
+}
