18651fdd04c572cdd2c3b449fcad0c8f2f1059af
rashid <rashidsiddqui07@gmail.com>
2020-05-08 15:49:49 +0530

    array introduction

---
+++ Array.java
@@ -0,0 +1,25 @@
+// Array ->array is collection of fixed index homegeneous element with fixed
+// collection concept
+// introduction
+// array declaration
+// array intialzation
+// array declaration, intialization in a single line
+// Anonymous aray
+// array element assigment
+// array variable assignment
+// array of array 
+// 2d array 
+// 3d array
+// [I
+// [B
+// 
+
+// public class Array {
+//     public static void main(String args[]) {
+//         // long b = 10l;
+//         int[] a = new int[];
+//         System.out.println(a.getClass().getName());
+
+//     }
+
+// }
